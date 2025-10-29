bab0cd2a7633cb0a92c388d85de368198479afbd
maarcosmrt-hub <maarcos.mrt@gmail.com>
2025-10-30 00:18:30 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,7 @@
-// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class', // no usamos oscuro, pero así no se activa por sistema
-  content: [
-    './public/index.html',
-    './src/**/*.{js,ts,jsx,tsx}',
-  ],
+  darkMode: 'class',
+  content: ['./public/index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: { extend: {} },
   plugins: [],
 };
