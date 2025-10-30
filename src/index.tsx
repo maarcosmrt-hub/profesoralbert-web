889ca2a649c95e4445bd6975af3a87c60a61c969
maarcosmrt-hub <maarcos.mrt@gmail.com>
2025-10-30 22:13:46 +0100

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
-import "./styles.css";
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+// src/index.tsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';            // ⬅️ IMPORTANTE: carga Tailwind
+import App from './App';
 
-const rootElement = document.getElementById("root")!;
-const root = ReactDOM.createRoot(rootElement);
-
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
 root.render(
   <React.StrictMode>
     <App />
